5a61f65728bfc5e0091ea56ae50d9c1d63b1e2f0
Adam Robson <adamray312@gmail.com>
2023-11-26 00:37:50 -0800

    add the old site archive and a header

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="w-screen flex justify-between items-baseline px-12">
+      <Link href={'/aboutus'}>About Us</Link>
+      <Link href={'/music'}>Music</Link>
+      <Link href={'/schedule'}>Schedule</Link>
+      <Link href={'/photos'}>Photos</Link>
+    </header>
+  )
+}

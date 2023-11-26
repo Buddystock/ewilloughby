c3ad806d892c374c001ecf84e8df2f08079fae77
Adam Robson <adamray312@gmail.com>
2023-11-26 00:54:38 -0800

    add title card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import Header from '../components/Header'
+import TitleCard from '@/components/TitleCard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+
+      <section className="my-10">
+        <Header />
+      </section>
+
+      <section className="mt-20">
+        <TitleCard />
+      </section>
+
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="w-screen flex justify-between items-baseline px-12">
+    <header className="w-screen flex justify-between items-baseline px-16">
       <Link href={'/aboutus'}>About Us</Link>
       <Link href={'/music'}>Music</Link>
       <Link href={'/schedule'}>Schedule</Link>

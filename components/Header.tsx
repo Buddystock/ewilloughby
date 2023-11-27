3206f747172437bff7fb062717e39e8b74d96b7d
Adam Robson <adamray312@gmail.com>
2023-11-26 17:43:54 -0800

    add fonts and background to landing page add styles and images

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className="w-screen flex justify-between items-baseline px-16">
-      <Link href={'/aboutus'}>About Us</Link>
-      <Link href={'/music'}>Music</Link>
-      <Link href={'/schedule'}>Schedule</Link>
-      <Link href={'/photos'}>Photos</Link>
+      <Link href={'/aboutus'} className="text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 hover:underline subpixel-antialiased">About Us</Link>
+      <Link href={'/music'} className="text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 hover:underline subpixel-antialiased">Music</Link>
+      <Link href={'/schedule'} className="text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 hover:underline subpixel-antialiased">Schedule</Link>
+      <Link href={'/photos'} className="text-base/4 sm:text-lg/5 md:text-xl/6 lg:text-2xl/7 hover:underline subpixel-antialiased">Photos</Link>
     </header>
   )
 }

15fa2159d171acdde3e63efbc813f8cb0af5eeec
Adam Robson <adamray312@gmail.com>
2023-11-29 23:32:16 -0800

    work on landing page add player

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import Image from 'next/image'
 import Header from '../components/Header'
 import TitleCard from '@/components/TitleCard'
+import AudioPlayer from '@/components/AudioPlayer'
 
 export default function Home() {
+
+  const songs = [
+    {
+      title: 'Living Room',
+      url: '/songs/livingroom.mp3',
+    },
+    {
+      title: 'Song 2',
+      url: '/songs/moonmission.mp3',
+    }
+  ]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
 
@@ -12,6 +25,10 @@ export default function Home() {
 
       <section className="mt-20">
         <TitleCard />
+      </section>
+
+      <section className="player">
+        <AudioPlayer songs={songs} />
       </section>
 
     </main>
